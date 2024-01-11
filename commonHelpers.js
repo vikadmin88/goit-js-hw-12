020fe7ff99c2a99c3bba17b0f37a5988b407de72
vikadmin88 <viktork8888@gmail.com>
2024-01-11 14:07:51 +0000

    Deploying to gh-pages from @ vikadmin88/goit-js-hw-12@0acd2f1dc653cb19db1b1eaf8665d14c63aece26 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,25 +1,29 @@
-import{i as a,S as u}from"./assets/vendor-46aac873.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))t(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&t(n)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function t(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}})();a.settings({position:"topRight",timeout:5e3,resetOnHover:!0,transitionIn:"flipInX",transitionOut:"flipOutX"});const m="https://pixabay.com/api/",p="41671607-e33db59ab0332d081087354c8",l=document.querySelector(".search-form"),c=document.querySelector(".loader"),f=document.querySelector(".gallery"),d=new u(".gallery a",{captionsData:"alt",captionDelay:250});l.addEventListener("submit",y);function y(o){o.preventDefault();const s=l.elements.searchStr.value.trim();if(!s){l.reset(),a.error({message:"The search field cannot be empty!"});return}f.innerHTML="",c.style.display="block";const i=new URLSearchParams({key:p,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:30,q:s});fetch(`${m}?${i}`).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()}).then(t=>{if(c.style.display="none",t.hits.length==0){a.warning({message:"Sorry, there are no images matching<br> your search query.Please try again!"});return}h(t.hits)}).catch(t=>a.error({message:`Api request error: ${t}`})).finally(()=>{l.reset()})}function h(o){f.insertAdjacentHTML("beforeend",o.map(g).join("")),d.refresh()}const g=({largeImageURL:o,webformatURL:s,tags:i,likes:t,views:e,comments:r,downloads:n})=>`
+import{i as c,S as g,a as h}from"./assets/vendor-89feecc5.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const r of o.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&a(r)}).observe(document,{childList:!0,subtree:!0});function i(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerpolicy&&(o.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?o.credentials="include":e.crossorigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function a(e){if(e.ep)return;e.ep=!0;const o=i(e);fetch(e.href,o)}})();c.settings({position:"topRight",timeout:5e3,resetOnHover:!0,transitionIn:"flipInX",transitionOut:"flipOutX"});const b=new g(".gallery a",{captionsData:"alt",captionDelay:250}),x="41671607-e33db59ab0332d081087354c8",v=`https://pixabay.com/api/?key=${x}`,n={orientation:"horizontal",image_type:"photo",safesearch:!0,per_page:40,page:1,q:""},l=document.querySelector(".search-form"),u=document.querySelector("div[data-loader='search']"),m=document.querySelector(".gallery"),p=document.querySelector("button[data-pagination"),f=document.querySelector("div[data-loader='pagination']"),d=document.querySelector(".nomore-msg");p.addEventListener("click",q);l.addEventListener("submit",S);function S(s){s.preventDefault();const t=l.elements.searchStr.value.trim();if(!t){l.reset(),c.error({message:"Search field cannot be empty!"});return}m.textContent="",u.style.display="block",p.style.display="none",d.style.display="none",n.q=t,n.page=1,y(n),l.reset()}function q(){f.style.display="block",y(n)}async function y(s){try{const i=(await h.get(v,{params:s})).data;if(i.hits.length==0){c.warning({message:"Sorry, there are no images matching<br> your search query.Please try again!"});return}P(i.hits),L(i.totalHits)}catch(t){c.error({message:`Api request error: ${t}`})}}function L(s){if(s<=n.per_page*n.page?(p.style.display="none",d.style.display="block"):(n.page+=1,p.style.display="block",d.style.display="none"),u.style.display="none",f.style.display="none",n.page>2){const t=m.children[0].getBoundingClientRect().height;window.scrollBy({left:0,top:t*2+20,behavior:"smooth"})}}function P(s){m.insertAdjacentHTML("beforeend",s.map(w).join("")),b.refresh()}const w=({largeImageURL:s,webformatURL:t,tags:i,likes:a,views:e,comments:o,downloads:r})=>`
     <li class="gallery-item">
-      <a class="gallery-link" href="${o}">
-        <img class="gallery-image" src="${s}" alt="${i}" />
-        <ul class="metabox-list">
-          <li class="mbox-info">
-            <p class="mbox-info-title">Likes</p>
-            <span class="mbox-info-descr">${t}</span>
-          </li>
-          <li class="mbox-info">
-            <p class="mbox-info-title">Views</p>
-            <span class="mbox-info-descr">${e}</span>
-          </li>
-          <li class="mbox-info">
-            <p class="mbox-info-title">Comments</p>
-            <span class="mbox-info-descr">${r}</span>
-          </li>
-          <li class="mbox-info">
-            <p class="mbox-info-title">Downloads</p>
-            <span class="mbox-info-descr">${n}</span>
-          </li>
-        </ul>
+      <a class="gallery-link" href="${s}">
+        <div class="item-wrap">
+          <div class="image-wrap">
+            <img class="gallery-image" src="${t}" alt="${i}" />
+          </div>
+          <ul class="metabox-list">
+            <li class="mbox-info">
+              <p class="mbox-info-title">Likes</p>
+              <span class="mbox-info-descr">${a}</span>
+            </li>
+            <li class="mbox-info">
+              <p class="mbox-info-title">Views</p>
+              <span class="mbox-info-descr">${e}</span>
+            </li>
+            <li class="mbox-info">
+              <p class="mbox-info-title">Comments</p>
+              <span class="mbox-info-descr">${o}</span>
+            </li>
+            <li class="mbox-info">
+              <p class="mbox-info-title">Downloads</p>
+              <span class="mbox-info-descr">${r}</span>
+            </li>
+          </ul>
+        </div>
       </a>
     </li>`;
 //# sourceMappingURL=commonHelpers.js.map
